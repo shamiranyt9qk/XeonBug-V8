73af450d598ea7a28aad9214e43115378457f237
shamiranyt9qk <shamiransaikia130@gmail.com>
2024-10-25 22:35:46 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,15 +10,15 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "923444844060"
-global.ownername = "Qasim Ali"
+global.ownernomer = "918011103411"
+global.ownername = "SHAMIRAN💦"
 global.ytname = "YT: GlobalTechInfo"
 global.socialm = "GitHub: GlobalTechInfo"
 global.location = "Pakistan, Karachi, Karachi"
 
-global.ownernumber = '923444844060'  //creator number
-global.ownername = 'Qasim Ali' //owner name
-global.botname = 'XeonBug-V8' //name of the bot
+global.ownernumber = '918011103411'  //creator number
+global.ownername = 'SHAMIRAN💦💦' //owner name
+global.botname = 'OOPS!!' //name of the bot
 
 //sticker details
 global.packname = 'Sticker By'
